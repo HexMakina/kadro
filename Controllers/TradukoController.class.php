3379df451d2c725befa0bea20e39981a06ebbf4f
sam <sammy@hexmakina.be>
2021-07-29 14:42:58 +0200

    initialisation process for i18n cache files, part of the setup feature

--- Controllers/TradukoController.class.php
+++ Controllers/TradukoController.class.php
@@ -7,6 +7,8 @@ use \HexMakina\Format\File\Text\JSON;
 
 class TradukoController extends \HexMakina\kadro\Controllers\ORMController
 {
+  const JSON_FILENAME = 'user_interface.json';
+
   public function authorize($permission = null)
   {
     return parent::authorize('group_admin');
@@ -19,6 +21,15 @@ class TradukoController extends \HexMakina\kadro\Controllers\ORMController
 
   public function update_file($lang='fra')
   {
+    $locale_path = $this->box('settings.locale_data_path');
+    self::create_file($locale_path, $lang);
+
+    $this->logger()->nice('KADRO_SYSTEM_FILE_UPDATED', [$report_filename]);
+    $this->router()->hop('traduko');
+  }
+
+  public static function create_file($locale_path, $lang)
+  {
     $res = Traduko::filter(['lang' => $lang]);
     $assoc = [];
     foreach($res as $id => $trad)
@@ -27,12 +38,25 @@ class TradukoController extends \HexMakina\kadro\Controllers\ORMController
       $assoc[$trad->kategorio][$trad->sekcio] = $assoc[$trad->kategorio][$trad->sekcio] ?? [];
       $assoc[$trad->kategorio][$trad->sekcio][$trad->referenco] = $trad->$lang;
     }
+    // test directory access
+    $path_to_file = $locale_path.'/'.$lang;
 
-    $report_filename = $lang.'/user_interface.json';
-    $file = new JSON(APP_BASE.'locale/'.$report_filename, 'w+');
-    $file->set_content(JSON::from_php($assoc));
+    try{
+      if(!JSON::exists($path_to_file))
+        JSON::make_dir($path_to_file);
 
-    $this->logger()->nice('KADRO_SYSTEM_FILE_UPDATED', [$report_filename]);
-    $this->router()->hop('traduko');
+      $file = new JSON($path_to_file.'/'.self::JSON_FILENAME, 'w+');
+      $file->set_content(JSON::from_php($assoc));
+    }catch(\Exception $e)
+    {
+      ddt($e);
+    }
+  }
+
+  public static function init($locale_path)
+  {
+    $languages = array_keys(array_slice(Traduko::inspect(Traduko::table_name())->columns(), 4));
+    foreach($languages as $l)
+      self::create_file($locale_path, $l);
   }
 }

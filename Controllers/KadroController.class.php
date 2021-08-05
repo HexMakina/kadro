52e9f65deac985f7c18b740f6d023df4d0da753c
sam <sammy@hexmakina.be>
2021-08-05 16:33:07 +0200

    Logger interface out of kadro

--- Controllers/KadroController.class.php
+++ Controllers/KadroController.class.php
@@ -6,7 +6,7 @@ use \Psr\Container\{ContainerInterface,ContainerExceptionInterface,NotFoundExcep
 
 use \HexMakina\kadro\Auth\{OperatorInterface, AccessRefusedException};
 use \HexMakina\kadro\Router\RouterInterface;
-use \HexMakina\kadro\Logger\LoggerInterface;
+use \HexMakina\Logger\LoggerInterface;
 use \HexMakina\Crudites\Interfaces\TracerInterface;
 
 class KadroController implements Interfaces\DisplayController

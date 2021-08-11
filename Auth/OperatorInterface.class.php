df11d1fd585dfb10e1ed2c150f20425c48fc7529
sam <sammy@hexmakina.be>
2021-08-11 11:49:57 +0200

    Operator interface updated with permission function

--- Auth/OperatorInterface.class.php
+++ Auth/OperatorInterface.class.php
@@ -5,6 +5,7 @@ namespace HexMakina\kadro\Auth;
 interface OperatorInterface
 {
   public function is_active() : bool;
+  public function is_new() : bool;
 
 
   public function operator_id();
@@ -18,4 +19,6 @@ interface OperatorInterface
   public function language_code();
   public function email();
   public function name();
+
+  public function has_permission($p) : bool
 }

388f8c8d8b393ee9b3ea1bef8b71eae746ae3191
sam <sammy@hexmakina.be>
2021-08-16 14:16:44 +0200

    made $context param optional for adding errors

--- Controllers/Interfaces/BaseControllerInterface.class.php
+++ Controllers/Interfaces/BaseControllerInterface.class.php
@@ -21,7 +21,7 @@ interface BaseControllerInterface
   public function conclude();
 
   public function errors() : array;
-  public function add_error($message, $context);
+  public function add_error($message, $context=[]);
 
   public function has_route_back() : bool;
   public function route_back($route_name=null, $route_params=[]) : string;

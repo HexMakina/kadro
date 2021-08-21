25f58b4d7bfec03abbeb138d4770c200e82475ae
sam <sammy@hexmakina.be>
2021-08-21 21:51:23 +0200

    Cleanup: interfaces for CRUD Controllers and ORM Controllers

--- Controllers/Interfaces/CRUDControllerInterface.php
+++ Controllers/Interfaces/CRUDControllerInterface.php
@@ -5,10 +5,17 @@ namespace HexMakina\kadro\Controllers\Interfaces;
 interface CRUDControllerInterface
 {
     public function dashboard();
-    public function edit();
-    public function save();
-    public function listing();
 
+    // creation & edition form (GET)
+    public function edit();
+    // creation & edition persistence (POST)
+    public function save();
+    // listings (GET)
+    public function listing();
+    
+    // deletion confirmation form (GET)
+    public function destroy_confirm();
+    // deletion persistence (POST)
     public function destroy();
 
     public function errors(): array;
--- Controllers/Interfaces/ORMControllerInterface.php
+++ Controllers/Interfaces/ORMControllerInterface.php
@@ -7,19 +7,6 @@ interface ORMControllerInterface extends CRUDControllerInterface
     // ORM class tranlator
     public function class_name(): string;
 
-    // creation & edition form (GET)
-    public function edit();
-    // creation & edition persistence (POST)
-    public function save();
-    // listings (GET)
-    public function listing();
-
-    // deletion confirmation form (GET)
-    public function destroy_confirm();
-    // deletion persistence (POST)
-    public function destroy();
-
-    public function dashboard();
     public function export();
     public function copy();
 

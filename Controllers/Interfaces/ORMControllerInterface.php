d93564c4af515e0466e9592286f8b6c7f7292f00
sam <sammy@hexmakina.be>
2021-08-21 21:47:08 +0200

    fixed syntax error

--- Controllers/Interfaces/ORMControllerInterface.php
+++ Controllers/Interfaces/ORMControllerInterface.php
@@ -5,7 +5,7 @@ namespace HexMakina\kadro\Controllers\Interfaces;
 interface ORMControllerInterface extends CRUDControllerInterface
 {
     // ORM class tranlator
-    public function class_name(): string
+    public function class_name(): string;
 
     // creation & edition form (GET)
     public function edit();

9b2c028aa6f9e5a0570c97383836c19e99cab0fc
sam <sammy@hexmakina.be>
2021-08-29 00:38:01 +0200

    moved: CRUD interface to ORM, CRUD is no more

--- Controllers/Interfaces/ORMControllerInterface.php
+++ Controllers/Interfaces/ORMControllerInterface.php
@@ -4,7 +4,7 @@ namespace HexMakina\kadro\Controllers\Interfaces;
 
 use HexMakina\TightORM\Interfaces\ModelInterface;
 
-interface ORMControllerInterface extends CRUDControllerInterface
+interface ORMControllerInterface
 {
     // ORM class tranlator
     public function class_name(): string;
@@ -13,8 +13,26 @@ interface ORMControllerInterface extends CRUDControllerInterface
     public function load_model(): ?ModelInterface;
     public function form_model(): ModelInterface;
 
+    public function dashboard();
+
+    // creation & edition form (GET)
+    public function edit();
+
+    // creation & edition persistence (POST)
+    public function save();
+
+    // deletion confirmation form (GET)
+    public function destroy_confirm();
+    // deletion persistence (POST)
+    public function destroy();
+
+    // listings (GET)
+    public function listing($model = null, $filters = [], $options = []);
+
+
     public function export();
     public function copy();
 
+    public function errors(): array;
     public function add_errors($errors);
 }

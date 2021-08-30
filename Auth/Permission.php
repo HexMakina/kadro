2e2c67a94ac2ca1b46191e06b41a8f3f2311ebc6
sam <sammy@hexmakina.be>
2021-08-31 00:17:43 +0200

    new: permissions constants GROUP_ADMIN, GROUP_STAFF, GROUP_MEDIC

--- Auth/Permission.php
+++ Auth/Permission.php
@@ -7,6 +7,10 @@ class Permission extends \HexMakina\TightORM\TightModel
     const TABLE_NAME = 'kadro_permission';
     const TABLE_ALIAS = 'permission';
 
+    const GROUP_ADMIN = 'group_admin';
+    const GROUP_STAFF = 'group_social';
+    const GROUP_MEDIC = 'group_medical';
+
     public function __toString()
     {
         return $this->get('name');

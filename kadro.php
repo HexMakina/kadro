3c50e3994e34b3ea13f3ac3c5d1839b926c1a416
sam <sammy@hexmakina.be>
2021-09-01 23:36:40 +0200

    refactor extracted routes array to routes.php

--- kadro.php
+++ kadro.php
@@ -66,33 +66,10 @@ class kadro
 
     private static function routing()
     {
-        $Hup = new \HexMakina\Hopper\hopper();
+        $Hup = new \HexMakina\Hopper\hopper(require(__DIR__.'/routes.php'));
         $Hup->setBasePath(self::$box->get('settings.hopper.web_base'));
         $Hup->setFilePath(self::$box->get('settings.hopper.file_root'));
         $Hup->mapHomeRoute(self::$box->get('settings.hopper.route_home'));
-
-        $Hup->addRoutes([
-          ['GET', 'checkin', 'ReceptionController::checkin', 'checkin'],
-          ['GET', 'checkout', 'ReceptionController::checkout', 'checkout'],
-          ['POST', 'identify', 'ReceptionController::identify', 'identify'],
-          ['GET', 'operator/[*:username]/toggle/active', 'OperatorController::change_active', 'operator_change_active'],
-          ['GET', 'operator/[*:username]/change-acl/[i:permission_id]', 'OperatorController::change_acl', 'acl_toggle'],
-          // --- SEARCH
-          ['POST|GET', 'search', 'SearchController::results', 'search'],
-
-          // --- TRADUKO
-          ['POST', 'traduko/update_file', 'TradukoController::update_file', 'traduko_update_file'],
-
-          // --- LOCALE JSON
-          ['GET', 'locale/language_codes.[a:format]', 'ExportController::otto_languages', 'otto_languages'],
-
-          // --- EXPORT
-          ['GET', 'export', 'ExportController::dashboard', 'export'], // default ExportController is dashboard
-          ['GET', 'otto/language_codes.[a:format]/term/[a:search]?', 'ExportController::otto_languages', 'otto_languages_search'],
-          ['GET', 'otto/[a:model]/distinct/[*:field].[a:format]', 'ExportController::otto_distinct_field', 'otto_distinct_field'],
-          ['GET', 'otto/[a:model]/distinct/[*:field].[a:format]/term/[*:search]?', 'ExportController::otto_distinct_field', 'otto_distinct_field_where'],
-          ['GET', 'export/[*:action].[a:format]', 'ExportController::dynamic_action_call', 'export_action_call'],
-        ]);
         return $Hup;
     }
 
---
+++ routes.php
@@ -0,0 +1,23 @@
+<?php
+return [
+  ['GET', 'checkin', 'ReceptionController::checkin', 'checkin'],
+  ['GET', 'checkout', 'ReceptionController::checkout', 'checkout'],
+  ['POST', 'identify', 'ReceptionController::identify', 'identify'],
+  ['GET', 'operator/[*:username]/toggle/active', 'OperatorController::change_active', 'operator_change_active'],
+  ['GET', 'operator/[*:username]/change-acl/[i:permission_id]', 'OperatorController::change_acl', 'acl_toggle'],
+  // --- SEARCH
+  ['POST|GET', 'search', 'SearchController::results', 'search'],
+
+  // --- TRADUKO
+  ['POST', 'traduko/update_file', 'TradukoController::update_file', 'traduko_update_file'],
+
+  // --- LOCALE JSON
+  ['GET', 'locale/language_codes.[a:format]', 'ExportController::otto_languages', 'otto_languages'],
+
+  // --- EXPORT
+  ['GET', 'export', 'ExportController::dashboard', 'export'], // default ExportController is dashboard
+  ['GET', 'otto/language_codes.[a:format]/term/[a:search]?', 'ExportController::otto_languages', 'otto_languages_search'],
+  ['GET', 'otto/[a:model]/distinct/[*:field].[a:format]', 'ExportController::otto_distinct_field', 'otto_distinct_field'],
+  ['GET', 'otto/[a:model]/distinct/[*:field].[a:format]/term/[*:search]?', 'ExportController::otto_distinct_field', 'otto_distinct_field_where'],
+  ['GET', 'export/[*:action].[a:format]', 'ExportController::dynamic_action_call', 'export_action_call'],
+];

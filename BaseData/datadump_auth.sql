5f831fe83a5b1ba83b5d71115f7ae8ada0a2af03
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-01-06 14:23:42 +0100

    added permissions and ACL for default user

--- BaseData/datadump_auth.sql
+++ BaseData/datadump_auth.sql
@@ -1,5 +1,5 @@
 START TRANSACTION;
-
 INSERT INTO `kadro_operator` (`id`, `username`, `name`) VALUES (1, 'kadro','kadro');
-
+INSERT INTO `kadro_permission` (`id`, `name`) VALUES (1, 'root'),(2, 'admin'),(3, 'user');
+INSERT INTO `kadro_acl` (`operator_id`, `permission_id`) VALUES (1, 1);
 COMMIT;

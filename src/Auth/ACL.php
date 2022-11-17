3d6f2dcb07ff7e558a8e9ea411a3dd6af37c8e8a
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-17 23:00:52 +0100

    Applied rules:  * SimplifyUselessVariableRector  * VarConstantCommentRector  * NewlineAfterStatementRector  * RemoveUselessParamTagRector  * RenameVariableToMatchMethodCallReturnTypeRector  * RenameParamToMatchTypeRector  * RenameVariableToMatchNewTypeRector  * RenameForeachValueVariableToMatchExprVariableRector  * AddArrayReturnDocTypeRector  * ReturnTypeDeclarationRector

--- src/Auth/ACL.php
+++ src/Auth/ACL.php
@@ -8,7 +8,14 @@ use HexMakina\Crudites\Queries\AutoJoin;
 
 class ACL extends \HexMakina\TightORM\TightModel
 {
+    /**
+     * @var string
+     */
     public const TABLE_NAME = 'kadro_acl';
+
+    /**
+     * @var string
+     */
     public const TABLE_ALIAS = 'acl';
 
     public function traceable(): bool
@@ -16,42 +23,43 @@ class ACL extends \HexMakina\TightORM\TightModel
         return false;
     }
 
-    public static function match(OperatorInterface $op, $permission_name)
+    public static function match(OperatorInterface $operator, $permission_name): bool
     {
-        return in_array($permission_name, self::permissions_names_for($op));
+        return in_array($permission_name, self::permissions_names_for($operator));
     }
 
     public static function query_retrieve($filters = [], $options = []): SelectInterface
     {
         $options['eager'] = false;
-        $ret = parent::query_retrieve($filters, $options);
+        $select = parent::query_retrieve($filters, $options);
         $eager_params = [];
         $eager_params[Permission::relationalMappingName()] = Permission::tableAlias();
         $eager_params[Operator::relationalMappingName()] = Operator::tableAlias();
         $eager_params[ACL::relationalMappingName()] = ACL::tableAlias();
 
         // why ? why dont you comment.. is the real question
-        AutoJoin::eager($ret, $eager_params);
+        AutoJoin::eager($select, $eager_params);
 
-        return $ret;
+        return $select;
     }
 
-    public static function permissions_for(OperatorInterface $op)
+    /**
+     * @return mixed[]
+     */
+    public static function permissions_for(OperatorInterface $operator): array
     {
-        $res = self::any(['operator_id' => $op->getId()]);
+        $res = self::any(['operator_id' => $operator->getId()]);
 
         $permission_ids = [];
-        foreach ($res as $r) {
-            $permission_ids[] = $r->get('permission_id');
+        foreach ($res as $re) {
+            $permission_ids[] = $re->get('permission_id');
         }
-
-        $ret = Permission::filter(['ids' => $permission_ids]);
-        return $ret;
+        return Permission::filter(['ids' => $permission_ids]);
     }
 
-    public static function permissions_names_for(OperatorInterface $op)
+    public static function permissions_names_for(OperatorInterface $operator)
     {
-        $operator_with_perms = get_class($op)::exists($op->getId());
+        $operator_with_perms = get_class($operator)::exists($operator->getId());
         // $operator_with_perms = get_class($op)::retrieve($operator_with_perms);
         if (is_null($operator_with_perms)) {
             return [];
@@ -60,12 +68,12 @@ class ACL extends \HexMakina\TightORM\TightModel
         return explode(',', $operator_with_perms->get('permission_names'));
     }
 
-    public static function allow_in(OperatorInterface $op, Permission $p)
+    public static function allow_in(OperatorInterface $operator, Permission $permission): \HexMakina\kadro\Auth\ACL
     {
-        $ret = new ACL();
-        $ret->set('operator_id', $op->getId());
-        $ret->set('permission_id', $p->getId());
-        $ret->save($op->getId());
-        return $ret;
+        $acl = new ACL();
+        $acl->set('operator_id', $operator->getId());
+        $acl->set('permission_id', $permission->getId());
+        $acl->save($operator->getId());
+        return $acl;
     }
 }

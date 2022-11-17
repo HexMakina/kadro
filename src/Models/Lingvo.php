f33c691a60d15838fbffe72fb0698981676e8fad
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-17 23:04:54 +0100

    Applied rules:  * VarConstantCommentRector  * NewlineBeforeNewAssignSetRector  * NewlineAfterStatementRector  * SymplifyQuoteEscapeRector  * RemoveUselessParamTagRector  * RenameVariableToMatchMethodCallReturnTypeRector  * AddArrayReturnDocTypeRector  * ReturnTypeDeclarationRector

--- src/Models/Lingvo.php
+++ src/Models/Lingvo.php
@@ -7,22 +7,69 @@ use HexMakina\TightORM\TightModel;
 
 class Lingvo extends TightModel
 {
+    /**
+     * @var string
+     */
     public const TABLE_NAME = 'kartisto_ISO639';
 
+    /**
+     * @var string
+     */
     public const ISO_3 = 'Part3';
+
+    /**
+     * @var string
+     */
     public const ISO_2B = 'Part2B';
+
+    /**
+     * @var string
+     */
     public const ISO_2T = 'Part2T';
+
+    /**
+     * @var string
+     */
     public const ISO_1 = 'Part1';
+
+    /**
+     * @var string
+     */
     public const ISO_NAME = 'Ref_Name';
 
+    /**
+     * @var string
+     */
     public const ISO_SET = 'Set';
+
+    /**
+     * @var array<string, string>
+     */
     public const ISO_SETS = [self::ISO_1 => 'ISO 639-1', self::ISO_3 => 'ISO 639-3', self::ISO_2B => 'ISO 639-2B', self::ISO_2T => 'ISO 639-2T'];
+
+    /**
+     * @var string
+     */
     public const ISO_DEFAULT = self::ISO_3;
 
+    /**
+     * @var string
+     */
     public const ISO_SCOPE = 'Scope';
+
+    /**
+     * @var array<string, string>
+     */
     public const ISO_SCOPES = ['I' => 'Individual', 'M' => 'Macrolanguage', 'S' => 'Special'];
 
+    /**
+     * @var string
+     */
     public const ISO_TYPE = 'Type';
+
+    /**
+     * @var array<string, string>
+     */
     public const ISO_TYPES = ['A' => 'Ancient', 'C' => 'Constructed', 'E' => 'Extinct', 'H' => 'Historical', 'L' => 'Living', 'S' => 'Special'];
 
 
@@ -41,12 +88,12 @@ class Lingvo extends TightModel
         return $this->get('Ref_Name');
     }
 
-    public function iso_scope()
+    public function iso_scope(): string
     {
         return self::ISO_SCOPES[$this->get('scope')];
     }
 
-    public function iso_type()
+    public function iso_type(): string
     {
         return self::ISO_TYPES[$this->get('scope')];
     }
@@ -57,7 +104,7 @@ class Lingvo extends TightModel
 
         $searchable_fields = [self::ISO_NAME, self::ISO_3, self::ISO_2B, self::ISO_2T, self::ISO_1];
 
-        $Query = static::table()->select();
+        $select = static::table()->select();
 
         if (isset($filter['name'])) {
         }
@@ -66,31 +113,35 @@ class Lingvo extends TightModel
         }
 
         if (isset($filter['term'])) {
-            $Query->whereFilterContent(['term' => $filter['term'], 'fields' => $searchable_fields], $Query->tableLabel(), 'OR');
+            $select->whereFilterContent(['term' => $filter['term'], 'fields' => $searchable_fields], $select->tableLabel(), 'OR');
         }
 
         if (isset($filter['requires_authority']) && isset(self::ISO_SETS[$filter['requires_authority']])) {
-            $Query->whereNotEmpty($filter['requires_authority']);
+            $select->whereNotEmpty($filter['requires_authority']);
         }
 
         if (isset($filter['types'])) {
-            $wc = sprintf("AND " . self::ISO_TYPE . " IN ('%s') ", implode('\', \'', array_keys(self::ISO_TYPES)));
-            $Query->where($wc);
+            $wc = sprintf("AND " . self::ISO_TYPE . " IN ('%s') ", implode("', '", array_keys(self::ISO_TYPES)));
+            $select->where($wc);
         }
+
         if (isset($filter['scopes'])) {
-            $wc = sprintf("AND " . self::ISO_SCOPE . " IN ('%s') ", implode('\', \'', array_keys(self::ISO_SCOPES)));
-            $Query->where($wc);
+            $wc = sprintf("AND " . self::ISO_SCOPE . " IN ('%s') ", implode("', '", array_keys(self::ISO_SCOPES)));
+            $select->where($wc);
         }
 
-        $Query->orderBy([self::TABLE_NAME, self::ISO_1, 'DESC']);
-        $Query->orderBy([self::TABLE_NAME, self::ISO_2T, 'DESC']);
-        $Query->orderBy([self::TABLE_NAME, self::ISO_3, 'DESC']);
-        $Query->orderBy([self::TABLE_NAME, self::ISO_NAME, 'DESC']);
+        $select->orderBy([self::TABLE_NAME, self::ISO_1, 'DESC']);
+        $select->orderBy([self::TABLE_NAME, self::ISO_2T, 'DESC']);
+        $select->orderBy([self::TABLE_NAME, self::ISO_3, 'DESC']);
+        $select->orderBy([self::TABLE_NAME, self::ISO_NAME, 'DESC']);
 
-        return $Query;
+        return $select;
     }
 
-    public static function search_language($term, $authority = null)
+    /**
+     * @return array<int|string, mixed>
+     */
+    public static function search_language($term, $authority = null): array
     {
         $rows = self::query_retrieve(['term' => $term, 'requires_authority' => $authority])->retAss();
         $ret = [];
@@ -105,6 +156,7 @@ class Lingvo extends TightModel
     {
         $Query = self::table()->select([self::ISO_NAME]);
         $Query->whereEQ(self::ISO_3, $code);
+
         $rows = $Query->retCol();
 
         if (isset($rows[0])) { // only 1 result

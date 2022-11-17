52cea4733a764dc8184799d50e7ba3b3cbb81a2a
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-17 23:04:05 +0100

    Applied rules:  * RemoveUselessParamTagRector  * AddArrayReturnDocTypeRector

--- src/Models/Interfaces/Searchable.php
+++ src/Models/Interfaces/Searchable.php
@@ -5,5 +5,8 @@ namespace HexMakina\kadro\Models\Interfaces;
 // implementation in Models\Abilities\Event
 interface Searchable
 {
+    /**
+     * @return mixed[]
+     */
     public function searchableFields(): array;
 }

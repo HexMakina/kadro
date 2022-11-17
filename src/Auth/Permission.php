7eca69c2994213f33128cbe21007146e663983f3
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-17 23:02:23 +0100

    Applied rules:  * VarConstantCommentRector  * NewlineAfterStatementRector

--- src/Auth/Permission.php
+++ src/Auth/Permission.php
@@ -4,11 +4,29 @@ namespace HexMakina\kadro\Auth;
 
 class Permission extends \HexMakina\TightORM\TightModel
 {
+    /**
+     * @var string
+     */
     public const TABLE_NAME = 'kadro_permission';
+
+    /**
+     * @var string
+     */
     public const TABLE_ALIAS = 'permission';
 
+    /**
+     * @var string
+     */
     public const GROUP_ADMIN = 'group_admin';
+
+    /**
+     * @var string
+     */
     public const GROUP_STAFF = 'group_social';
+
+    /**
+     * @var string
+     */
     public const GROUP_MEDIC = 'group_medical';
 
     public function __toString()

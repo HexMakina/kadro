45880f950cac1884132d7ffb0da2fb2f78433c75
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-04 18:26:00 +0100

    added too many methods.. will be reworked

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -12,23 +12,42 @@ class Tag extends TightModel
         return $this->get('label');
     }
 
+    public function reference(): string
+    {
+        return $this->get('reference');
+    }
+
+    public function slug(): string
+    {
+        return $this->get('reference');
+    }
+
+    public function label(): string
+    {
+        return $this->get('label');
+    }
+
+    public function content(): string
+    {
+        return $this->get('content') ?? '';
+    }
+
+    public static function queryListing($filters = [], $options = []): SelectInterface{
+        return self::query_retrieve($filters, $options);
+    }
+
     public static function query_retrieve($filters = [], $options = []): SelectInterface
     {
         //---- JOIN & FILTER SERVICE
-        $Query = parent::query_retrieve($filters, $options);
-        if(isset($filters['parent']))
-        {
-           if(is_numeric($filters['parent']))
-           {
-                $Query->whereEQ('id', $filters['parent'], 'parent');
-           }
-           else 
-           {
-                $Query->whereEQ('reference', $filters['parent'], 'parent');
-           }
+        $query = parent::query_retrieve($filters, $options);
+
+        if (isset($filters['parent'])) {
+            $column_name = is_numeric($filters['parent']) ? 'id' : 'reference';
+            $query->whereEQ($column_name, $filters['parent'], 'parent');
         }
 
-        $Query->orderBy('label');
-        return $Query;
+        $query->orderBy('label');
+        
+        return $query;
     }
 }
